61747c388fc965ca2229bffee5a6332b15c3282b
ThaysMrtns <thays.martins@nave.org.br>
2020-11-14 05:01:34 -0300

    Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  #controller#page
-  root 'pages#home'
-  #verb 'rout name', to: 'path'
-  get 'login', to: 'pages#login'
-  get 'register', to: 'pages#register'
   #Will give me all routes of the article
-  resources :article, only: [:show, :index, :new, :create, :edit, :update, :destroy] #But I want only the read operation
+  resources :article, only: [:show, :index, :new, :create, :edit, :update, :destroy] 
 end
